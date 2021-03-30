6cd99ecc5ff63c7ea1c1fa1acf65c35942ee6c51
zav930802 <zav930802@163.com>
2021-03-30 20:10:42 +0800

    1

--- msvc_msys2.bat
+++ msvc_msys2.bat
@@ -2,4 +2,4 @@
 
 (call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86 8.1 -vcvars_ver=14.1) || (pause & exit /B 1)
 
-
+(call "C:\msys64\msys2_shell.cmd" -mingw64 -use-full-path) || (pause & exit /B 1)
